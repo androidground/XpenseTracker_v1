5356dfd64d51e1a1bd971bbec788805d835dc8d6
androidground <vishal.saxena@hotmail.com>
2016-04-21 15:38:32 +0530

    income/expense toggle button

--- app/src/main/java/com/bitbytetech/xpensetracker/AddIncomeExpenseActivity.java
+++ app/src/main/java/com/bitbytetech/xpensetracker/AddIncomeExpenseActivity.java
@@ -6,8 +6,12 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class AddIncomeExpenseActivity extends AppCompatActivity {
+
+    private boolean incomeSelected;
+    private boolean expenseSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,33 @@ public class AddIncomeExpenseActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final Button incomeButton = (Button) findViewById(R.id.income_button);
+        final Button expenseButton = (Button) findViewById(R.id.expense_button);
+
+        View.OnClickListener topButtonsListener  = new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if (view.getId() == R.id.income_button){
+                    incomeButton.setSelected(true);
+                    expenseButton.setSelected(false);
+                    incomeSelected = true;
+                    expenseSelected = false;
+                }
+                else{
+                    incomeButton.setSelected(false);
+                    expenseButton.setSelected(true);
+                    incomeSelected = false;
+                    expenseSelected = true;
+                }
+            }
+        };
+
+        incomeButton.setOnClickListener(topButtonsListener);
+        expenseButton.setOnClickListener(topButtonsListener);
+        incomeButton.performClick();
     }
+
+
 
 }
